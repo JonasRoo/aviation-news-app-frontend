37f24720945c77c35ea3fc81712114c9f7f991e5
JonasRoo <jonas-roosen@t-online.de>
2021-01-06 18:22:00 +0100

    styling on heart

--- frontend/src/components/Article.tsx
+++ frontend/src/components/Article.tsx
@@ -1,11 +1,14 @@
-import { Avatar, Card, Tooltip, Image } from 'antd';
-import React from 'react';
+import { Avatar, Card, Tooltip, Image, Button, Space, message } from 'antd';
+import React, { useState } from 'react';
 import moment from 'moment';
 import './style/Article.css';
+import { HeartTwoTone, StopTwoTone } from '@ant-design/icons';
+import api from '../utils/api';
 
 const { Meta } = Card;
 
 export interface Props {
+	pk: number;
 	source: string;
 	title: string;
 	link: string;
@@ -18,17 +21,55 @@ export interface Props {
 }
 
 export const Article: React.FC<Props> = (props) => {
+	const [ heartVisible, setHeartVisible ] = useState<boolean>(false);
+	const [ isHearted, setIsHearted ] = useState<boolean>(false);
+
+	const heartButtonHandler = (): void => {
+		api
+			.post(`articles/${props.pk}/heart/`)
+			.then((res) => {
+				setIsHearted(!isHearted);
+				message.success(res.data.message);
+			})
+			.catch((err) => {
+				message.error('Error hearting article!');
+			});
+	};
+
+	const dateContent = (
+		<Space>
+			{isHearted && <HeartTwoTone twoToneColor="#eb2f96" />}
+			<Tooltip title={moment(props.date_published).format('L')}>
+				<span>{moment(props.date_published).fromNow()}</span>
+			</Tooltip>
+		</Space>
+	);
+
+	const heartContent = isHearted ? (
+		<Tooltip title="Remove heart">
+			<Button icon={<StopTwoTone twoToneColor="#eb2f96" />} shape="circle" block onClick={heartButtonHandler} />
+		</Tooltip>
+	) : (
+		<Tooltip title="Heart this article">
+			<Button
+				className="heart-icon"
+				icon={<HeartTwoTone twoToneColor="#eb2f96" />}
+				shape="circle"
+				block
+				onClick={heartButtonHandler}
+			/>
+		</Tooltip>
+	);
+
 	return (
 		<div className="card">
 			<Card
 				style={{ width: 'flex' }}
 				hoverable
+				onMouseEnter={() => setHeartVisible(true)}
+				onMouseLeave={() => setHeartVisible(false)}
 				size="small"
-				extra={
-					<Tooltip title={moment(props.date_published).format('L')}>
-						<span>{moment(props.date_published).fromNow()}</span>
-					</Tooltip>
-				}
+				extra={heartVisible ? heartContent : dateContent}
 				title={props.title}
 			>
 				<a href={props.link} target="_blank" rel="noreferrer">
@@ -38,13 +79,14 @@ export const Article: React.FC<Props> = (props) => {
 						description={props.description}
 					/>
 				</a>
-				{props.source_icon && props.source_name ? (
+				{props.source_icon &&
+				props.source_name && (
 					<a href={`https://${props.source_name}`} target="_blank" rel="noreferrer">
 						<Tooltip title={props.source_name}>
 							<Avatar className="source-avatar" size="large" src={<Image src={props.source_icon} preview={false} />} />
 						</Tooltip>
 					</a>
-				) : null}
+				)}
 			</Card>
 		</div>
 	);
